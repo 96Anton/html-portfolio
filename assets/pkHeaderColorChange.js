ebda0e52e030d1ccbc6005303b0d6a7db498a425
96Anton <ahlmanton@gmail.com>
2025-09-03 09:52:11 +0200

    Update pkHeaderColorChange.js
    js disco

--- assets/pkHeaderColorChange.js
+++ assets/pkHeaderColorChange.js
@@ -1,6 +1,8 @@
-
 let button = document.getElementById("colorButton");
 let header = document.getElementById("pageHeader");
+let H1 = document.querySelector("h1");
+let secondary = document.querySelector(".secondary");
+let h2 = document.querySelector("h2");
 
 let colors = [
   "#E8B89D",
@@ -13,9 +15,26 @@ let colors = [
   header.style.backgroundColor // keeps the original header bg
 ];
 
+
 let currentColorIndex = 0;
 
-button.addEventListener("click", function() {
+header.addEventListener("click", function() {
   currentColorIndex = (currentColorIndex + 1) % colors.length;
   header.style.backgroundColor = colors[currentColorIndex];
+  h2.style.backgroundColor = colors[currentColorIndex + 1];
+  secondary.style.backgroundColor = colors[currentColorIndex - 1];
 });
+
+h2.addEventListener("click", function() {
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
+  header.style.backgroundColor = colors[currentColorIndex];
+  h2.style.backgroundColor = colors[currentColorIndex + 1];
+  secondary.style.backgroundColor = colors[currentColorIndex - 1];
+} );
+
+secondary.addEventListener("click", function() {
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
+  header.style.backgroundColor = colors[currentColorIndex];
+  h2.style.backgroundColor = colors[currentColorIndex + 1];
+  secondary.style.backgroundColor = colors[currentColorIndex - 1];
+} );
