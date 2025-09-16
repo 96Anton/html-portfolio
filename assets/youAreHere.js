6905942be6e59cb97381056bb7bc4ef31a7ce28f
96Anton <ahlmanton@gmail.com>
2025-09-16 16:04:50 +0200

    Create youAreHere.js
    v7

---
+++ assets/youAreHere.js
@@ -0,0 +1,27 @@
+function youAreHere() {
+  const pages = {
+    "Startsida": "/index.html",
+    "Anton Ahlm CV": "/public/about.html",
+    "Kontakt": "/public/contact.html",
+    "Parkour": "/public/parkour.html",
+    "Savanna": "/public/sunnySavanna.html"
+  };
+
+  const currentTitle = document.title;
+
+  let html = "<ul>";
+  for (const [title, url] of Object.entries(pages)) {
+    if (title === currentTitle) {
+      html += `<li class="current"><a href="${url}">${title}</a></li>`;
+    } else {
+      html += `<li><a href="${url}">${title}</a></li>`;
+    }
+  }
+  html += "</ul>";
+
+  document.getElementById("here").innerHTML = html;
+}
+
+youAreHere();
+
+
